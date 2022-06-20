3f49b1edb0f610517d693d1288255dcd1e1acf6d
Hendrik Mans <hendrik@mans.de>
2022-06-20 09:00:17 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { Canvas } from "@react-three/fiber";
 export default function App() {
   return (
     <Canvas>
+      {/* Environment, lighting, etc. */}
       <Environment preset="sunset" />
       <Sky />
 
+      {/* User Controls */}
+      <OrbitControls />
+
+      {/* Actual Scene Contents */}
       <mesh>
         <dodecahedronGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
-
-      <OrbitControls />
     </Canvas>
   );
 }
